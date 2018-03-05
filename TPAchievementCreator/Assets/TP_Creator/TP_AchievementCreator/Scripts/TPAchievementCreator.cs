692b91259fef4eaf35b86bd4527f5bcb524f6a5e
Tomasz Piowczyk <tomasz.piowczyk@gmail.com>
2018-03-05 13:13:41 +0100

    Numerate + copyrights

--- TPAchievementCreator/Assets/TP_Creator/TP_AchievementCreator/Scripts/TPAchievementCreator.cs
+++ TPAchievementCreator/Assets/TP_Creator/TP_AchievementCreator/Scripts/TPAchievementCreator.cs
@@ -1,4 +1,11 @@
-﻿using System.Collections;
+﻿/**
+*   Authored by Tomasz Piowczyk
+*   Copyright 2018 You're allowed to make changes in functionality and use for commercial or personal.
+*   You're not allowed to claim ownership of this script. 
+*   https://github.com/Prastiwar/TPAchievementCreator
+*   Unity 2017.3.0f
+*/
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
